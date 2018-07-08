192bac5bc71167ca03fffc3b2816dd22aa8907b4
tsasaki609 <admin@easy-coding.xyz>
2018-07-08 19:40:53 +0900

    微妙なコメントだけどWHYを書くべしとかそれだけ言われてもねー

--- src/Geofence.kt
+++ src/Geofence.kt
@@ -1,5 +1,11 @@
 class Geofence(val poiLatitude: Double, val poiLongitude: Double, val fenceRadias: Double) {
     fun status(fromLatitude: Double, toLatitude: Double, fromLongitude: Double, toLongitude: Double): GeofenceEvent {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //始点はフェンスに入ってる？
+
+        //終点はフェンスに入ってる？
+
+        //2つの状態から、フェンスとの関係を求める
+
     }
 }
